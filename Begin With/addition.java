60fcc26592f6493c505e11b0af9e16caa26c99f2
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-13 22:08:48 +0530

    Update addition.java
    Added necessary comments

--- Begin With/addition.java
+++ Begin With/addition.java
@@ -1,7 +1,11 @@
 package com.itbulls.learnit.aswin.operations;
 
+//change package if necessary
+
 public class addition{
 
+/* program simply prints the sum of ttwo numbers */	
+	
 	public static void main(String[] args) {
 		float n1=(Integer.parseInt(args[0]));
 		float n2=(Integer.parseInt(args[2]));
