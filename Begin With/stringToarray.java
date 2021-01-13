0454d629d2d726e36b46b009d7a711714c39c7b4
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-13 22:13:01 +0530

    Update stringToarray.java
    Added necessary comments

--- Begin With/stringToarray.java
+++ Begin With/stringToarray.java
@@ -1,15 +1,23 @@
 package com.itbulls.learnit.aswin.operations;
+
+//change package if necessary!
+
 import java.util.Scanner;
+
 public class stringToarray {
+	
+/* program accepts an string from the user and create an array using the characters */	
 
 	public static void main(String[] args) {
 		System.out.println("Enter any text");
+		
 		Scanner sc= new Scanner(System.in);
 		String s= sc.next();
+		
 		char[] ch=s.toCharArray();
 		for (char c : ch) { 
-            System.out.print(c); 
-        } 
+			    System.out.print(c); 
+			} 
 	}
 
 }
