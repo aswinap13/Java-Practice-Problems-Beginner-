9a970f6b732fc790544b0c87bd7fd3a132ca97ef
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-14 16:02:59 +0530

    Update FindMaxInt.java
    Added necessary comments

--- Problems/FindMaxInt.java
+++ Problems/FindMaxInt.java
@@ -1,16 +1,22 @@
 package com.itbulls.learnit.aswin.operations;
 
+//change package if necessary!
+
 import java.util.Arrays;
 import java.util.Scanner;
 public class FindMaxInt {
+	
+/* program accepts number of inputs from the user and returns the maximum of it */	
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please, enter integer numbers separated by space: ");
 	
-	   String numbers=sc.nextLine();
-	   String numberstring[]=numbers.split(" ");
-	   int[] intArray=new int[numberstring.length];
+	   String numbers=sc.nextLine(); //scanning the numbers in to string
+	   String numberstring[]=numbers.split(" "); // converting string to string array  [split is to split the string at special points delimitter which is mentioned inside]
+		
+	   int[] intArray=new int[numberstring.length]; //converting the string array into an integer array
+		
 	   for(int i=0;i<numberstring.length;i++) {
 		   intArray[i]=Integer.parseInt(numberstring[i]);
 	   }
@@ -25,6 +31,9 @@ public class FindMaxInt {
 
 	
 	public static int findMaxIntInArray(int[] intArray) {
+		
+		//finding maximum among the array
+		
 		int maxInt=0;
 		for(int i=0;i<intArray.length;i++)
 		{
