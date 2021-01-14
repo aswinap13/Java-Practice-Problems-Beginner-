4473d1139eff2a698fb4b69e625172f5d49c0f4f
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-14 17:03:13 +0530

    Update AmountOfWords.java
    Added comments

--- Problems/AmountOfWords.java
+++ Problems/AmountOfWords.java
@@ -1,6 +1,11 @@
 package com.itbulls.learnit.aswin.operations;
+
+//change package if necessary
+
 import java.util.Scanner;
 public class AmountOfWords {
+	
+/* program calculates the number of words in a string input by the user */
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -18,3 +23,23 @@ public class AmountOfWords {
 		return str.length;
 	}
 }
+
+/* 
+ Consider the below code! it don't use array or any additional conversions - this is the simplified one of above code
+
+public class AmountOfWords {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please, enter any text: ");
+		String userInput = sc.nextLine();
+		
+		int amountOfWords = getWordsAmount(userInput);
+		System.out.println("Amount of words in your text: " + amountOfWords);
+	}
+
+	public static int getWordsAmount(String text) {
+		return text.split("[\\p{P}\\s]+").length;
+	}
+}
+*/
