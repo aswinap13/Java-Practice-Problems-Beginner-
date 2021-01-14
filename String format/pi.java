0a8b1c291a7fdebb3e1ef5ff91e0b74847b4e65a
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-14 08:46:48 +0530

    Add files via upload

---
+++ String format/pi.java
@@ -0,0 +1,15 @@
+package com.itbulls.learnit.aswin.operations;
+
+import java.math.*;
+public class pi {
+
+	public static void main(String[] args) {
+		
+		System.out.println("double : "+ String.format("%.1f", Math.PI));
+		System.out.println("double : "+ String.format("%.2f", Math.PI));
+		System.out.println("double : "+ String.format("%.3f", Math.PI));
+		System.out.println("double : "+ String.format("%.4f", Math.PI));
+		System.out.println("double : "+ String.format("%.5f", Math.PI));
+	}
+
+}
