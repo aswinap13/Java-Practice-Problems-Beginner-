5c47385818936ff69729dac677ccc9ed90faae83
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-14 11:03:09 +0530

    Update PatternPyramid.java
    Added necessary comments

--- Pattern Printing/PatternPyramid.java
+++ Pattern Printing/PatternPyramid.java
@@ -1,6 +1,25 @@
 package com.itbulls.learnit.aswin.operations;
+
+//change the package if necessary
+
 import java.util.Scanner;
+
 public class PatternPyramid {
+	
+/* 	Program prints the following pattern depending upon the input user gives :
+				* 
+				* * 
+				* * * 
+				* * * * 
+				* * * * * 
+				* * * * * * 
+				* * * * * 
+				* * * * 
+				* * * 
+				* * 
+				* 
+	 	 	Here the user input = 6    				*/
+
 
 	public static void main(String[] args) {
 		
@@ -10,12 +29,16 @@ public class PatternPyramid {
 		
 		int n=scan.nextInt();
 		
+		//For printing the upper incrementing pattern
+		
 		for (int i = 0; i < n ; i++) {
 			for (int j = 0; j <= i ; j++) {
 			  System.out.print("* ");
 			}
 			System.out.println();
 		}
+		
+		// For printing the lower reducing pattern
 		for(int p=0;p<n-1;p++) {
 			for(int q=n-1;q>p;q--) {
 				System.out.print("* ");
