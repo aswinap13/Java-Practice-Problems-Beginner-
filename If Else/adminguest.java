507540e59dd0d1b39208dd6934a68171ceb4b6ae
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-14 08:43:30 +0530

    Update and rename adguest.java to adminguest.java
    Added necessary comments

--- If Else/adminguest.java
+++ If Else/adminguest.java
@@ -1,17 +1,23 @@
 package com.itbulls.learnit.aswin.operations;
 
-public class adguest {
+//change package if necessary!
 
+public class adminguest{
+
+/* program accepts arguments from user(either admin or guest) and act accordingly */	
+	
 	public static void main(String[] args) {
+		
 		String inputArguments= String.join(",", args);
 		String adminin="--admin";
 		String guestin="--guest";
+		
 		if(inputArguments.contains(adminin)&& inputArguments.contains(guestin))
 			System.out.println("Please select either admin or guest for this program");
 		else if(inputArguments.contains(adminin))
 			System.out.println("Helo Admin");
 		else if(inputArguments.contains(guestin))
-			System.out.println("Helo GUest");
+			System.out.println("Helo Guest");
 		else
 			System.out.println("NULL");
 		
