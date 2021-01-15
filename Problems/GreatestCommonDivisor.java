5225d6d89c92f174a6c17491ad5542c294a6648e
Aswin <69101976+aswinap13@users.noreply.github.com>
2021-01-15 06:44:34 +0530

    Update GreatestCommonDivisor.java
    Added comments

--- Problems/GreatestCommonDivisor.java
+++ Problems/GreatestCommonDivisor.java
@@ -1,10 +1,13 @@
 package com.itbulls.learnit.aswin.operations;
 
+//change package if necessary!
+
 import java.util.Scanner;
 
 public class GreatestCommonDivisor {
 	
-
+/* program accepts two numbers as input from the user and returns the greatest common divisor of them */
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please, enter two numbers separated by space: ");
@@ -19,7 +22,7 @@ public class GreatestCommonDivisor {
 	
 	public static int gcdRecursive(int firstNumber, int secondNumber) {
 		if (secondNumber == 0) {
-			
+			// in case if first number is negetive(assuming user input numbers in order)
 			return Math.abs(firstNumber); 
 		} else {
 			return gcdRecursive(secondNumber, firstNumber % secondNumber);
